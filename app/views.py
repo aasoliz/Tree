ed44614772f6a44ca3571c3485f560daf5b7e35b
Andrea Soliz <and.sol17@gmail.com>
2015-04-30 21:31:37 -0500

    database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
+from flask import render_template, url_for, flash, redirect
 from app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'nickname': 'Andrea'}
+  posts = [
+      {
+          'author': {'nickname': 'Andrea'},
+          'body': 'Hello, everybody!'
+      },
+      {
+          'author': {'nickname': 'Kaylene'},
+          'body': 'Hey, Andrea!'
+      }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+      flash('Login requested for userID="%s", remember_me=%s' %
+            (form.loginID.data, str(form.remember_me.data)))
+      return redirect(url_for('index'))
+  return render_template('login.html', title='Sign In', form=form)
---
+++ db_downgrade.py
@@ -0,0 +1,11 @@
+#!venv/bin/python
+
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from migrate.versioning import api
+
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+
+print('Current database version: ' + str(v))
